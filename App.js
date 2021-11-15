2f1a5060c207105d9b8e73f2720910e1a9b901c7
YEAjiJUNG <happyhan96@naver.com>
2021-11-15 20:16:14 +0900

    react-native 공부

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import * as Location from 'expo-location';
+import {Text, View, StyleSheet, ScrollView, Dimensions, ActivityIndicator} from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
+
+const {width: SCREEN_WIDTH} = Dimensions.get('window');
 
 export default function App() {
+  const [city, setCity] = useState('Loading...');
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const API_KEY = '4cbf83c8c1ca06a1e27106a88090142c';
+  const icons = {
+    Clouds: "cloudy",
+    Clear: "day-sunny",
+    Atmosphere: "cloudy-gusts",
+    Snow: "snow",
+    Rain: "rains",
+    Drizzle: "rain",
+    Thunderstorm: "lightning",
+  }
+
+  const getWeather = async() => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+
+    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5});
+    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
+    setCity(location[0].city);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`)
+    const json = await response.json();
+    setDays(json.daily);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false} contentContainerStyle={styles.weather}>
+        {days.length === 0 ? (
+          <View style={styles.day} >
+            <ActivityIndicator color="white" style={{marginTop:10}} size="large"/>
+          </View>
+        )  : (
+          days.map((day, index) =>
+            <View key={index} style={styles.day} >
+              <View style={{flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'space-between'}}>
+                <Text style={styles.temp}>{parseFloat(day.temp.day.toFixed(1))}</Text>
+                <Fontisto name={icons[day.weather[0].main]} size={38} color="white" />
+              </View>
+
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          )
+        )}
+      </ScrollView>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'skyblue'
   },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    fontSize: 50,
+    fontWeight: '500'
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    marginTop: 50,
+  },
+  temp: {
+    fontSize: 120,
+    marginLeft: 20,
+  },
+  description: {
+    marginTop: -10,
+    marginLeft: 20,
+    fontSize: 50
+  },
+  tinyText: {
+    marginLeft: 20,
+    fontSize: 20,
+  }
 });
